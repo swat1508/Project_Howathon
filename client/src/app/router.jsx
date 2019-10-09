c98e23f389dc5269a0f705eda3177173373c83bb
Sourav Mondal <sourav.mondal@publicissapient.com>
2019-10-10 00:57:29 +0530

    [ADD] router gaurds

--- client/src/app/router.jsx
+++ client/src/app/router.jsx
@@ -1,42 +1,51 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {Provider} from 'react-redux'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
-// import PWALoadable from './components/pwa-loadable'
-import Login from './pages/login'
-import Home from './pages/home'
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Provider} from 'react-redux';
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
+// import PWALoadable from './components/pwa-loadable';
+import Login from './pages/login';
+import Home from './pages/home';
 
-// const Login = PWALoadable(() => import('./pages/login'))
-// const Home = PWALoadable(() => import('./pages/home'))
+// const Login = PWALoadable(() => import('./pages/login'));
+// const Home = PWALoadable(() => import('./pages/home'));
 
 class Router extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            loggedIn: true
+        };
+    }
 
     render() {
-        const {store} = this.props
-        // return (
-        //     <Provider store={store}>
-        //         <BrowserRouter>
-        //             <Route path='/' component={Login}>
-        //                 <Route path="login" component={Login} />
-        //                 <Route path="home" component={Home} />
-        //             </Route>
-        //         </BrowserRouter>
-        //     </Provider>
-        // )
+        const {store} = this.props;
 
         return (
             <Provider store={store}>
                 <BrowserRouter>
                     <Switch>
-                        <Route path="/login">
-                            <Login />
-                        </Route>
-                        <Route path="/home">
-                            <Home />
-                        </Route>
-                        <Route exact path="/">
-                            <Login />
-                        </Route>
+                        <Route exact path="/login" render={() => (
+                            this.state.loggedIn ? (
+                                <Redirect to="/home"/>
+                            ) : (
+                                <Login />
+                            )
+                            )}/>
+                        <Route exact path="/home" render={() => (
+                            this.state.loggedIn ? (
+                                <Home />
+                            ) : (
+                                <Redirect to="/login" />
+                            )
+                            )}/>
+                        <Route exact path="/" render={() => (
+                            this.state.loggedIn ? (
+                                <Redirect to="/"/>
+                            ) : (
+                                <Login />
+                            )
+                            )}/>
                     </Switch>
                 </BrowserRouter>
             </Provider>
