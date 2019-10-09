a7e8dd07f1275f5b26ef5ef8455067148783ee1d
Brajesh Anokha <anokha777@gmail.com>
2019-10-09 12:40:27 +0530

    docker completed

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,11 @@
 if (process.env.NODE_ENV == 'production') {
   console.log('PROD', process.env.NODE_ENV);
   module.exports = { mongoURI: 'mongodb://swat1508:sinha1508@ds129045.mlab.com:29045/eunoia-howathon' };
+  // For docker
+  // module.exports = {mongoURI: 'mongodb://mongo:27017/eunoia-howathon'};
 } else {
   console.log('NON_PROD', process.env.NODE_ENV);
   module.exports = { mongoURI: 'mongodb://localhost/eunoia-howathon' };
+  // For docker
+  // module.exports = {mongoURI: 'mongodb://mongo:27017/eunoia-howathon'};
 }
