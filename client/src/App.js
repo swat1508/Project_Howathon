58a55b2e012c4827a448e0085f8c8cc4e682d545
Vinita Gupta <vinita.gupta@publicissapient.com>
2019-10-09 12:32:10 +0530

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React from "react";
+import "./App.scss";
 
-import { Button, ButtonToolbar } from 'react-bootstrap';
+import { Button, ButtonToolbar } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="login-hero-img"></div>
 
-        <ButtonToolbar>
-          <Button variant="outline-primary">Primary</Button>
-          <Button variant="outline-secondary">Secondary</Button>
-          <Button variant="outline-success">Success</Button>
-          <Button variant="outline-warning">Warning</Button>
-          <Button variant="outline-danger">Danger</Button>
-          <Button variant="outline-info">Info</Button>
-          <Button variant="outline-light">Light</Button>
-          <Button variant="outline-dark">Dark</Button>
-        </ButtonToolbar>
+        {/* <ButtonToolbar>
+          <Button variant="outline-success">Sign-in with Google</Button>
+        </ButtonToolbar> */}
+        <div className="google-login-button">
+                    
+          <a
+            href="https://backend-xt-fsd.herokuapp.com/users/auth/google"
+            class="button"
+          >
+                        
+            <button class="loginBtn loginBtn--google">Login with Google</button>
+                        
+          </a>
+                  
+        </div>
       </header>
     </div>
   );
