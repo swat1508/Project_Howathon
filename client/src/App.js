272616d52f988a2f9619baf49a209ba8c718c3f7
Swatantra Sinha <swasinha1@publicisgroupe.net>
2019-10-09 10:22:57 +0530

    makig homepage

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+             <header className="App-header">
+                
+                <h1 className="App-title">Welcome to ChatApp by EUNOIA </h1>
+            </header>
+ 
+<div className="googleAuthFormCss">
+      Please Sign in below
+
+      <div className="google-login-button">
+          <a href="https://backend-xt-fsd.herokuapp.com/users/auth/google" class="button">
+            <button class="loginBtn loginBtn--google">Login with Google</button>
+            </a>
+        </div>
+
+
+      </div>
       </div>
     );
   }
