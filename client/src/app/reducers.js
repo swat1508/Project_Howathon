373b51e344c3005f95a1c5935c51192b276ea4e3
mohiit1502 <mohiit1502@gmail.com>
2019-10-10 01:04:26 +0530

    Added user details in state

--- client/src/app/reducers.js
+++ client/src/app/reducers.js
@@ -1,12 +1,13 @@
 import {combineReducers} from 'redux'
 import homeReducer from './pages/home/reducer.js'
+import loginReducer from './pages/login/reducer.js'
 // Reducers
-
 
 export default combineReducers({
     ui: combineReducers({
         pages: combineReducers({
             home: homeReducer,
+            login: loginReducer
         })
     }),
 })
