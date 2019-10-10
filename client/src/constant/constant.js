7145a7c1af4e36ef7c4ef0a95f76a8ab422cb22b
Brajesh Anokha <anokha777@gmail.com>
2019-10-10 07:36:53 +0530

    auth url

--- client/src/constant/constant.js
+++ client/src/constant/constant.js
@@ -1,2 +1,4 @@
-export const serverUrl = 'http://localhost:4001';
-export const frontendUrl = 'http://localhost:3000';
+// export const serverUrl = 'http://localhost:4001';
+// export const frontendUrl = 'http://localhost:3000';
+export const serverUrl = 'http://ec2-52-90-159-146.compute-1.amazonaws.com:4001';
+export const frontendUrl = 'http://ec2-52-90-159-146.compute-1.amazonaws.com:3000';
